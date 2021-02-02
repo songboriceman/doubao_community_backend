f00849c6d6e67c986b7e867fe22d0efe5213ef3e
zhangsan <zhangsan@sohu.com>
2021-02-02 10:17:51 +0800

    init

---
+++ src/main/java/com/douyuehan/doubao/DoubaoApplication.java
@@ -0,0 +1,13 @@
+package com.douyuehan.doubao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoubaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoubaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/douyuehan/doubao/DoubaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douyuehan.doubao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoubaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
